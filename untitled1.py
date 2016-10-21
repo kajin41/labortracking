f4798854323d12cc5d6679bb610efe17de04736a
greg <kajin41@gmail.com>
2016-10-21 16:37:56 -0400

    1

--- untitled1.py
+++ untitled1.py
@@ -1,15 +1,20 @@
-from flask import Flask
-import pyodbc
+from flask import Flask, request, render_template
+#import pyodbc
+from datetime import datetime, timedelta
 app = Flask(__name__)
 
-conn = pyodbc.connect(
-    'DRIVER={SQL Server};;SERVER=cti-syspro.local.citytheatrical.com;PORT=51845;DATABASE=SysproCompanyZ;UID=CITY\gmercado;PWD=01189998819991197253')
+# conn = pyodbc.connect(
+#     'DRIVER={SQL Server};;SERVER=cti-syspro.local.citytheatrical.com;PORT=51845;DATABASE=SysproCompanyZ;UID=CITY\gmercado;PWD=01189998819991197253')
+#
+# cursor = conn.cursor()
 
-cursor = conn.cursor()
 
-
-@app.route('/')
+@app.route('/labor/tracking', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == 'GET':
+        job = {'number': '00058954', 'status': 'ON', 'time': datetime.now()}
+        employees = [{'name': 'Greg', 'time': datetime.now()}]
+        return render_template('Workstation.html', job=job, employees=employees)
     return 'Hello World!'
 
 
