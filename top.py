71d7df5d56bd0cb8efd1dc93ed6f927b7c1479c9
greg <kajin41@gmail.com>
2016-11-18 15:06:26 -0500

    f

--- top.py
+++ top.py
@@ -15,7 +15,7 @@ ipaddrs = {'10.1.1.34': 'FAB010',
            '10.1.1.79': 'gmercado'}
 
 SQLQueries = {
-    'station-manhours': "SELECT [TrackLaborJob].Job, [Station], sum(datediff(minute,Datestart,DateFinish)) as manMinutes\
+    'station-manhours': "SELECT [TrackLaborEmployee].Job, [Station], sum(datediff(minute,Datestart,DateFinish)) as manMinutes\
                           FROM [SysproCompanyC].[dbo].[TrackLaborEmployee]\
                           left join WipMaster\
                           on TrackLaborEmployee.Job=WipMaster.Job",
@@ -29,8 +29,8 @@ SQLQueries = {
                             FROM [SysproCompanyC].[dbo].[TrackLaborJob]\
                             left join WipMaster\
                             on TrackLaborJob.Job=WipMaster.Job",
-    'station-group': "GROUP BY MasterJob,Job,Station",
-    'subJob-manhours': "SELECT [TrackLaborJob].Job, sum(datediff(minute,Datestart,DateFinish)) as manMinutes\
+    'station-group': "GROUP BY MasterJob,WipMaster.Job,Station",
+    'subJob-manhours': "SELECT [TrackLaborEmployee].Job, sum(datediff(minute,Datestart,DateFinish)) as manMinutes\
                           FROM [SysproCompanyC].[dbo].[TrackLaborEmployee]\
                           left join WipMaster\
                           on TrackLaborEmployee.Job=WipMaster.Job",
@@ -42,8 +42,8 @@ SQLQueries = {
                             FROM [SysproCompanyC].[dbo].[TrackLaborJob]\
                             left join WipMaster\
                             on TrackLaborJob.Job=WipMaster.Job",
-    'subJob-group': "GROUP BY MasterJob, TrackLaborJob.Job",
-    'masterJob-manhours': "SELECT [TrackLaborJob].Job, sum(datediff(minute,Datestart,DateFinish)) as manMinutes\
+    'subJob-group': "GROUP BY MasterJob, WipMaster.Job",
+    'masterJob-manhours': "SELECT [TrackLaborEmployee].Job, sum(datediff(minute,Datestart,DateFinish)) as manMinutes\
                           FROM [SysproCompanyC].[dbo].[TrackLaborEmployee]\
                           left join WipMaster\
                           on TrackLaborEmployee.Job=WipMaster.Job",
