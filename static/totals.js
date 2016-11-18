45d6160edcd4f54e1779dcf381cec9b0b945d28f
greg <kajin41@gmail.com>
2016-11-18 09:46:35 -0500

    f

--- static/totals.js
+++ static/totals.js
@@ -1,9 +1,11 @@
 /**
  * Created by gmercado on 11/18/2016.
  */
-$(".filter").change(function (){
-  if (this.val() == "Open"){
-      document.getElementsByName("From").style.display = 'none';
-  }
-}
-);
+$(document).ready(function () {
+    $(".filter").change(function (){
+      if (this.val() == "Open"){
+          document.getElementsByName("From").style.display = 'none';
+      }
+    }
+    );
+});
