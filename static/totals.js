2039b8c31f7166ecb623e258516f00aa98c55116
greg <kajin41@gmail.com>
2016-11-18 10:06:08 -0500

    f

--- static/totals.js
+++ static/totals.js
@@ -8,10 +8,14 @@ $(document).ready(function () {
           document.getElementsByName("Todiv")[0].style.display = 'none';
       }
       else if (this.value == "Job"){
+          document.getElementsByName("From")[0].type = 'date';
+          document.getElementsByName("To")[0].type = 'date';
           document.getElementsByName("Fromdiv")[0].style.display = 'block';
           document.getElementsByName("Todiv")[0].style.display = 'block';
       }
       else{
+          document.getElementsByName("From")[0].type = 'text';
+          document.getElementsByName("To")[0].type = 'text';
           document.getElementsByName("Fromdiv")[0].style.display = 'block';
           document.getElementsByName("Todiv")[0].style.display = 'block';
       }
