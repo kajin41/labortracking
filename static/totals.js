531147e4903a78bc02750779590eed2f39032f8b
greg <kajin41@gmail.com>
2016-11-18 09:58:04 -0500

    f

--- static/totals.js
+++ static/totals.js
@@ -4,7 +4,16 @@
 $(document).ready(function () {
     $(".filter").change(function (){
       if (this.value == "Open"){
-          document.getElementsByName("From")[0].style.display = 'none';
+          document.getElementsByName("Fromdiv")[0].style.display = 'none';
+          document.getElementsByName("Todiv")[0].style.display = 'none';
+      }
+      else if (this.value == "Job"){
+          document.getElementsByName("Fromdiv")[0].style.display = 'block';
+          document.getElementsByName("Todiv")[0].style.display = 'block';
+      }
+      else{
+          document.getElementsByName("Fromdiv")[0].style.display = 'block';
+          document.getElementsByName("Todiv")[0].style.display = 'block';
       }
     }
     );
