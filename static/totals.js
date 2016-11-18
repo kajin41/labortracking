b104c3ab75a3f261dc9f10b42b707bf47df8ac6f
greg <kajin41@gmail.com>
2016-11-18 09:33:53 -0500

    f

--- static/totals.js
+++ static/totals.js
@@ -2,7 +2,7 @@
  * Created by gmercado on 11/18/2016.
  */
 $(".filter").change(function (){
-  if (this.val() == "open"){
+  if (this.val() == "Open"){
       document.getElementsByName("From").style.display = 'none';
   }
 }
